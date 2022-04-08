b5ecd1388c3ab5ca14f32098e2fe8bd36e0d6a8a
Fran Ribeiro <ribeirofrv@gmail.com>
2022-04-08 12:16:01 -0300

    adiciona 3 botões diferentes que quando clicando é exibido uma mensagem no consol

--- 2.Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2.Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,8 +5,16 @@ import './App.css';
 não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no botão!')
+function handleClickOne() {
+  console.log('Clicou no botão 1')
+}
+
+function handleClickTwo() {
+  console.log('Clicou no botão 2')
+}
+
+function handleClickThree() {
+  console.log('Clicou no botão 3')
 }
 
 class App extends React.Component {
@@ -14,7 +22,16 @@ class App extends React.Component {
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
   render() {
-    return <button onClick={handleClick}>Meu botão</button>
+    return (
+      <div className='App-header'>
+        <h1>Fancy Buttons</h1>
+        <div>
+          <button onClick={handleClickOne}>Meu botão 1</button>
+          <button onClick={handleClickTwo}>Meu botão 2</button>
+          <button onClick={handleClickThree}>Meu botão 3</button>
+        </div>
+      </div>
+    )
   }
 }
 
