2be965887642b8908a433a3f4122181feba38318
Fran Ribeiro <ribeirofrv@gmail.com>
2022-04-08 12:32:06 -0300

    refatora função handleClick que mostra mensagem no console

--- 2.Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2.Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import './App.css';
 
-/* Embora isso funcione, essa DEFINITIVAMENTE
-não é a maneira correta de se criar eventos
-em React! A função se refere ao componente,
-então deve ser parte de sua classe! */
-function handleClickOne() {
-  console.log('Clicou no botão 1')
-}
-
-function handleClickTwo() {
-  console.log('Clicou no botão 2')
-}
-
-function handleClickThree() {
-  console.log('Clicou no botão 3')
-}
-
 class App extends React.Component {
-  /* Repare que, diferentemente do HTML, no
-  JSX você associa uma função a um evento
-  passando a própria função entre chaves `{}` */
+  handleClick() {
+    console.log(`Clicou no botão`)
+  }
+  
   render() {
     return (
       <div className='App-header'>
         <h1>Fancy Buttons</h1>
         <div>
-          <button onClick={handleClickOne}>Meu botão 1</button>
-          <button onClick={handleClickTwo}>Meu botão 2</button>
-          <button onClick={handleClickThree}>Meu botão 3</button>
+          <button type="button" onClick={this.handleClick}>Meu botão 1</button>
+          {/* <button type="button" onClick={this.handleClick}>Meu botão 2</button>
+          <button type="button" onClick={this.handleClick}>Meu botão 3</button> */}
         </div>
       </div>
     )
